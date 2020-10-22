b29ed457a4e6c2fd9301416d5d757d2dc155b6d4
tarantot <roskishnyi@gmail.com>
2020-10-22 18:39:10 +0000

    no.1

---
+++ file.py
@@ -0,0 +1,14 @@
+import subprocess as sb
+
+data = sb.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+
+profiles = [i.split(':')[1][1:-1] for i in data if 'All User Profile' in i]
+
+for i in profiles:
+    results = sb.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+    results = [b.split(':')[1][1:-1] for b in data if 'Key Content' in b]
+
+    try:
+        print('{:<30}|   {:<}'.format(i, results[0]))
+    except IndexError:
+        print('{:<30}|   {:<}'.format(i, ''))
